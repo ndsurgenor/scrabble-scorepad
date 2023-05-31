9b076172a6a9cf6d1368b4ec5cf7f06ec1900054
Nathan Surgenor <ndsurgenor@gmail.com>
2023-05-31 21:28:16 +0000

    Create main and welcome_message functions

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def welcome_message():
+    """
+    Presents the user with an introductory message.
+    """
+    print('------------------------------')
+    print(' WELCOME TO SCRABBLE SCOREPAD')
+    print('------------------------------\n')
+
+    print('When presented with options, simply type the number of your choice and hit Enter\n')
+
+
+def main():
+    """
+    Run all functions within the program
+    """
+    welcome_message()
+
+
+main()
